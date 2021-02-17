9761f0379f825bb9bd87076f06cc22fc7019be1d
Pritesh Mantri <priteshpmantri@gmail.com>
2021-02-18 01:08:49 +0530

    Initial commit

--- Spotify/settings.py
+++ Spotify/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ow+e-3)xf67-*sw7j)zc8w*%lcbudxek*#o@n#7vqm84+l#-)9'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
